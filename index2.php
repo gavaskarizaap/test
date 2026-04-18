5f991c04a1b22d227bf883ea8f8c85169fcce3c3
gavaskarizaap <gavaskarizaap@gmail.com>
2026-04-18 19:56:29 +0530

    Second Commit

--- index2.php
+++ index2.php
@@ -20,7 +20,7 @@ try {
         $html = $pdf->html();
 
         // Output the HTML (or save it to a file)
-        echo $html;
+        echo $html."test";
     } else {
         // Handle the case where the PDF is empty
         echo "Error: The PDF document is empty or unreadable.";

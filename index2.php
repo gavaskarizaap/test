b4844caedefb8b46b13f6b87f2f929646a4e0b10
gavaskarizaap <gavaskarizaap@gmail.com>
2026-04-22 13:51:18 +0530

    Changes on 22-04-2026

--- index2.php
+++ index2.php
@@ -20,7 +20,7 @@ try {
         $html = $pdf->html();
 
         // Output the HTML (or save it to a file)
-        echo $html."test1231231";
+        echo $html."test1231231121";
     } else {
         // Handle the case where the PDF is empty
         echo "Error: The PDF document is empty or unreadable.";
